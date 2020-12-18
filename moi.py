c144696889e186af59ccc4691bd5953146010c2f
sangjinkal <sangjin.kal@gmail.com>
2020-12-18 23:53:05 +0900

    Delete moi.py

--- moi.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
- return 'See you in TeroKarvinen.com!\n\n'
-
-if __name__ == '__main__':
- app.run(host=localhost, port=5000)

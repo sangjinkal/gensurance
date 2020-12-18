71e418e98f54089f65040bcbe21ffede31fdbe6a
sangjinkal <sangjin.kal@gmail.com>
2020-12-18 23:54:49 +0900

    Delete __init__.py

--- openapi/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-# Name: /openapi/models
-from .dbconnect import connection,Base,db_session, init_db
-from sqlalchemy import Column, Integer, String, DateTime
-
-from .stocklist import StockList
-from .stocklookup import LookUp
